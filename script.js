5c5a54e7485d5148f0b3b71fb6322e9aa6b8c551
Ygor32 <ygor.b.32@gmail.com>
2024-09-09 21:31:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ checkoutBtn.addEventListener("click", function(){
     }).join("")
 
     const message = encodeURIComponent(cartItems)
-    const phone = "11951756822"
+    const phone = "111951756822"
 
     window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 
