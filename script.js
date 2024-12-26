cd8945199f54338baf04fd91c1117c3951ac7375
Ygor Botelho Mota <ygor.b.32@gmail.com>
2024-12-26 11:59:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 24;
+    return hora >= 10 && hora < 24;
     // true = restauranrte está aberto
 }
 
